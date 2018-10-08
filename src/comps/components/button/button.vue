47283b584386e0f2b5f92b4c004897c6b79808aa
a_boy <1471047476@qq.com>
2018-10-08 09:53:05 +0800

    提交ui框架

---
+++ src/comps/components/button/button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-button">
+    我是按钮
+  </div>
+</template>
+<style scoped>
+.w-button {
+  padding: 10px 20px;
+  color: #fff;
+  background: #409eff;
+  border-radius: 10px;
+}
+</style>
+<script>
+
+export default {
+  name: 'w-button'
+}
+
+</script>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import wUI from './comps/index'
+Vue.use(wUI)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
